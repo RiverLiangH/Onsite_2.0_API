34588f2a02b27bcc152794ed217747c66f892cf0
River <1306715855@qq.com>
2023-02-03 15:22:24 +0800

    database connected
    Signed-off-by: River <1306715855@qq.com>

---
+++ se-proj-rear-end/src/main/java/com/evan/seprojrearend/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.evan.seprojrearend.mapper;
+
+import com.evan.seprojrearend.po.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ se-proj-rear-end/src/main/java/com/evan/seprojrearend/po/User.java
@@ -0,0 +1,33 @@
+package com.evan.seprojrearend.po;
+
+public class User {
+    private Integer userid;
+
+    private String username;
+
+    private String mobile;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+}
