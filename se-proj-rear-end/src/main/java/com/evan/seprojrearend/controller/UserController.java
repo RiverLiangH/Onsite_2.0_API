909f738351c49d1fe4422ac85339c7bcba5dab25
jiaqiLv <1031903858@qq.com>
2023-02-13 22:47:53 +0800

    token

--- se-proj-rear-end/src/main/java/com/evan/seprojrearend/controller/UserController.java
+++ se-proj-rear-end/src/main/java/com/evan/seprojrearend/controller/UserController.java
@@ -10,6 +10,8 @@ package com.evan.seprojrearend.controller;
 import com.evan.seprojrearend.common.JsonResult;
 import com.evan.seprojrearend.po.User;
 import com.evan.seprojrearend.service.UserService;
+import com.evan.seprojrearend.utils.TokenUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Api(tags="用户信息模块")
@@ -35,17 +38,28 @@ public class UserController {
      * **/
     @ApiOperation(value="用户登录验证",notes="请输入用户名和密码")
     @ResponseBody
-    @GetMapping("login")
+    @PostMapping("login")
     public JsonResult loginValidate(String username, String password){
         String re = null;
+        String message = null;
+        String token = null;
         //re = userService.isInUser(username, password);
         try {
             re = userService.isInUser(username, password);
+            //用户信息存在
+            if(re.equals("True")){
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(password);
+                token= TokenUtils.sign(user);    //登录验证
+            }
+            message = re + ":" +token;
         }catch (Exception e){
             return JsonResult.isError(10001,"未知错误");
         }
-        return JsonResult.isOk(re);
+        return JsonResult.isOk(message);
     }
+
 
     /**
      * 用户注册
